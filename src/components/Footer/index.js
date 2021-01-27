01359a544d428c5aac493795f2837c184cd1ed8a
hamza bellamine <hamza.bellamine@gmail.com>
2021-01-27 17:10:51 +0100

    react router + custom layouts + footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles.scss'
+
+const Footer = (props) => {
+  return (
+    <footer className="footer">
+      <div className="footer">
+        <div className="wrap">HB 2020</div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
