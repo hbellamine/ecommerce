062442673e5374c8a258fe657bfe2d274df3fedb
hamza bellamine <hamza.bellamine@gmail.com>
2021-01-27 17:01:19 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./default.scss";
-import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
+
+//layouts
+import MainLayout from "./layouts/MainLayout";
+import HomepageLayout from "./layouts/HomepageLayout";
+//pages
 function App() {
   return (
     <div className="App">
-        <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+      {/* //switch will render the first route that matches */}
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )}
+        />
+        <Route
+          path="/registration"
+          render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
