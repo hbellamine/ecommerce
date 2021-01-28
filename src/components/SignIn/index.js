ab2b0afca88aa3686c44b99dddbd2f4804d705e9
hamza bellamine <hamza.bellamine@gmail.com>
2021-01-28 13:01:21 +0100

    Sign in Email - password

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,31 +1,79 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Button from "./../Forms/Button";
 import "./styles.scss";
-import { signInWithGoogle } from "./../../firebase/utils";
+import { signInWithGoogle, auth } from "./../../firebase/utils";
+import FormInput from "./../Forms/FormInput";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 class SignIn extends Component {
-    handleSubmit = async e => {
-        //to prevent the reloading of the page when the form is submitted
-        e.preventDefault();
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit = async (e) => {
+    //to prevent the reloading of the page when the form is submitted
+    e.preventDefault();
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email,password);
+      this.setState({
+        ...initialState
+      })
+    } catch (err) {
+      //console.log(err)
     }
-    render(){
-        return (
-            <div className="signin">
-              <div className="wrap">
-                <h2>Login</h2>
-                <div className="formWrap">
-                  <form onSubmit={this.handleSubmit}>
-                    <div className="socialSignin">
-                      <div className="row">
-                        <Button onClick={signInWithGoogle}>Sign in with google</Button>
-                      </div>
-                    </div>
-                  </form>
+  };
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div className="signin">
+        <div className="wrap">
+          <h2>Login</h2>
+          <div className="formWrap">
+            <form onSubmit={this.handleSubmit}>
+              <FormInput
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email"
+                handleChange={this.handleChange}
+              />
+              <FormInput
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Password"
+                handleChange={this.handleChange}
+              />
+
+              <Button type="submit">LogIn</Button>
+              <div className="socialSignin">
+                <div className="row">
+                  <Button onClick={signInWithGoogle}>
+                    Sign in with google
+                  </Button>
                 </div>
               </div>
-            </div>
-          );
-    }
-  
-};
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default SignIn;
