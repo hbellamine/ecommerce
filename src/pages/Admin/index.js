d9b78f148790b04824c3d210f5af068ff10b5a6a
hamza bellamine <hamza.bellamine@gmail.com>
2021-02-01 15:11:49 +0100

    fetching products from firebase + deleting products"

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,53 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addProductStart } from './../../redux/Products/products.actions';
-import Modal from './../../components/Modal';
-import FormInput from './../../components/Forms/FormInput';
-import FormSelect from './../../components/Forms/FormSelect';
-import Button from './../../components/Forms/Button';
-import LoadMore from './../../components/LoadMore';
-import CKEditor from 'ckeditor4-react';
-import './styles.scss';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addProductStart,
+  fetchProductsStart,
+  deleteProductStart
+} from "./../../redux/Products/products.actions";
+import Modal from "./../../components/Modal";
+import FormInput from "./../../components/Forms/FormInput";
+import FormSelect from "./../../components/Forms/FormSelect";
+import Button from "./../../components/Forms/Button";
+import LoadMore from "./../../components/LoadMore";
+import CKEditor from "ckeditor4-react";
+import "./styles.scss";
 
-// const mapState = ({ productsData }) => ({
-//   products: productsData.products
-// });
+const mapState = ({ productsData }) => ({
+  products: productsData.products,
+});
 
-const Admin = props => {
-//   const { products } = useSelector(mapState);
+const Admin = (props) => {
+  const { products } = useSelector(mapState);
   const dispatch = useDispatch();
   const [hideModal, setHideModal] = useState(true);
-  const [productCategory, setProductCategory] = useState('mens');
-  const [productName, setProductName] = useState('');
-  const [productThumbnail, setProductThumbnail] = useState('');
+  const [productCategory, setProductCategory] = useState("mens");
+  const [productName, setProductName] = useState("");
+  const [productThumbnail, setProductThumbnail] = useState("");
   const [productPrice, setProductPrice] = useState(0);
-  const [productDesc, setProductDesc] = useState('');
+  const [productDesc, setProductDesc] = useState("");
 
-//   const { data, queryDoc, isLastPage } = products;
+  //   const { data, queryDoc, isLastPage } = products;
 
   useEffect(() => {
-    // dispatch(
-    //   fetchProductsStart()
-    // );
+    dispatch(fetchProductsStart());
   }, []);
 
   const toggleModal = () => setHideModal(!hideModal);
 
   const configModal = {
     hideModal,
-    toggleModal
+    toggleModal,
   };
 
   const resetForm = () => {
     setHideModal(true);
-    setProductCategory('mens');
-    setProductName('');
-    setProductThumbnail('');
+    setProductCategory("mens");
+    setProductName("");
+    setProductThumbnail("");
     setProductPrice(0);
-    setProductDesc('');
+    setProductDesc("");
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     dispatch(
@@ -60,7 +62,6 @@ const Admin = props => {
       })
     );
     resetForm();
-
   };
 
   const handleLoadMore = () => {
@@ -78,13 +79,10 @@ const Admin = props => {
 
   return (
     <div className="admin">
-
       <div className="callToActions">
         <ul>
           <li>
-            <Button onClick={() => toggleModal()}>
-              Add new product
-            </Button>
+            <Button onClick={() => toggleModal()}>Add new product</Button>
           </li>
         </ul>
       </div>
@@ -92,35 +90,35 @@ const Admin = props => {
       <Modal {...configModal}>
         <div className="addNewProductForm">
           <form onSubmit={handleSubmit}>
-
-            <h2>
-              Add new product
-            </h2>
+            <h2>Add new product</h2>
 
             <FormSelect
               label="Category"
-              options={[{
-                value: "mens",
-                name: "Mens"
-              }, {
-                value: "womens",
-                name: "Womens"
-              }]}
-              handleChange={e => setProductCategory(e.target.value)}
+              options={[
+                {
+                  value: "mens",
+                  name: "Mens",
+                },
+                {
+                  value: "womens",
+                  name: "Womens",
+                },
+              ]}
+              handleChange={(e) => setProductCategory(e.target.value)}
             />
 
             <FormInput
               label="Name"
               type="text"
               value={productName}
-              handleChange={e => setProductName(e.target.value)}
+              handleChange={(e) => setProductName(e.target.value)}
             />
 
             <FormInput
               label="Main image URL"
               type="url"
               value={productThumbnail}
-              handleChange={e => setProductThumbnail(e.target.value)}
+              handleChange={(e) => setProductThumbnail(e.target.value)}
             />
 
             <FormInput
@@ -130,74 +128,65 @@ const Admin = props => {
               max="10000.00"
               step="0.01"
               value={productPrice}
-              handleChange={e => setProductPrice(e.target.value)}
+              handleChange={(e) => setProductPrice(e.target.value)}
             />
 
             <CKEditor
-              onChange={evt => setProductDesc(evt.editor.getData())}
+              onChange={(evt) => setProductDesc(evt.editor.getData())}
             />
 
             <br />
 
-            <Button type="submit">
-              Add product
-            </Button>
-
+            <Button type="submit">Add product</Button>
           </form>
         </div>
       </Modal>
 
       <div className="manageProducts">
-
         <table border="0" cellPadding="0" cellSpacing="0">
           <tbody>
             <tr>
               <th>
-                <h1>
-                  Manage Products
-                </h1>
+                <h1>Manage Products</h1>
               </th>
             </tr>
             <tr>
               <td>
-                <table className="results" border="0" cellPadding="10" cellSpacing="0">
-                  {/* <tbody>
-                    {(Array.isArray(data) && data.length > 0) && data.map((product, index) => {
+                <table
+                  className="results"
+                  border="0"
+                  cellPadding="10"
+                  cellSpacing="0"
+                >
+                  <tbody>
+                    {products.map((product, index) => {
                       const {
                         productName,
                         productThumbnail,
                         productPrice,
                         documentID
                       } = product;
-
                       return (
-                        <tr key={index}>
+                        <tr>
                           <td>
-                            <img className="thumb" src={productThumbnail} />
+                            <img key={index} src={productThumbnail} />
                           </td>
+                          <td>{productName}</td>
+                          <td>${productPrice}</td>
                           <td>
-                            {productName}
-                          </td>
-                          <td>
-                            £{productPrice}
-                          </td>
-                          <td>
-                       
-                            <Button onClick={() => {}}> 
-                              Delete
+                            <Button onClick={()=>dispatch(deleteProductStart(documentID))}>
+                                Delete
                             </Button>
                           </td>
                         </tr>
-                      )
+                      );
                     })}
-                  </tbody>*/}
-                </table> 
+                  </tbody>
+                </table>
               </td>
             </tr>
             <tr>
-              <td>
-
-              </td>
+              <td></td>
             </tr>
             <tr>
               <td>
@@ -216,11 +205,9 @@ const Admin = props => {
             </tr>
           </tbody>
         </table>
-
       </div>
-
     </div>
   );
-}
+};
 
 export default Admin;
--- src/redux/Products/products.sagas.js
+++ src/redux/Products/products.sagas.js
@@ -1,42 +1,64 @@
-import {takeLatest,put,all,call} from 'redux-saga/effects'
-import {handleAddProduct} from './products.helpers'
-import productsTypes from './products.types'
-import {auth} from './../../firebase/utils'
-//we need to listen to action and update redux store 
+import { takeLatest, put, all, call } from "redux-saga/effects";
+import { handleAddProduct, handleFetchProducts,handleDeleteProduct } from "./products.helpers";
+import { setProducts, fetchProductsStart } from "./products.actions";
+import productsTypes from "./products.types";
+import { auth } from "./../../firebase/utils";
+//we need to listen to action and update redux store
 
-//generative function 
+//generative function
 
-export function* addProduct({payload: {
-    productCategory,
-    productName,
-    productThumbnail,
-    productPrice
-}}){
+export function* addProduct({
+  payload: { productCategory, productName, productThumbnail, productPrice },
+}) {
+  try {
+    const timestamp = new Date();
+    yield handleAddProduct({
+      productCategory,
+      productName,
+      productThumbnail,
+      productPrice,
+      productAdminUserUID: auth.currentUser.uid,
+      createdDate: timestamp,
+    });
+    yield put(fetchProductsStart());
+  } catch (err) {
+    //console.log(err)
+  }
+}
 
+export function* onAddProductStart() {
+  yield takeLatest(productsTypes.ADD_NEW_PRODUCT_START, addProduct);
+}
 
-    try {
-        const timestamp = new Date()
-        yield handleAddProduct({
-            productCategory,
-            productName,
-            productThumbnail,
-            productPrice,
-            productAdminUserUID: auth.currentUser.uid,
-            createdDate:timestamp
-        })
-    } catch (err){
+export function* fetchProducts() {
+  try {
+    const products = yield handleFetchProducts();
+    yield put(setProducts(products));
+  } catch (err) {
+    //console.log(err)
+  }
+}
+
+export function* onFetchProductsStart() {
+  yield takeLatest(productsTypes.FETCH_PRODUCTS_START, fetchProducts);
+}
+
+export function* deleteProduct({payload}){
+
+    try{
+        yield handleDeleteProduct(payload)
+        yield put(
+            fetchProductsStart()
+        )
+    } catch(err){
         //console.log(err)
     }
 }
 
-export function* onAddProductStart () {
-
-    yield takeLatest(productsTypes.ADD_NEW_PRODUCT_START,addProduct)
+export function* onDeleterProductStart(){
+    yield takeLatest(productsTypes.DELETE_PRODUCT_START,deleteProduct)
 }
 
-
-export default function* productsSagas(){
-    yield all([
-        call(onAddProductStart)
-    ])
+export default function* productsSagas() {
+  yield all([call(onAddProductStart), call(onFetchProductsStart), call(onDeleterProductStart)]);
 }
