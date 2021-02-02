022d2d3452cbaf38b5cf2ac825da97eb552e0a73
hamza bellamine <hamza.bellamine@gmail.com>
2021-02-02 13:08:50 +0100

    adding home and search elements on the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { useSelector,useDispatch } from "react-redux";
-import {signOutUserStart} from './../../redux/User/user.actions'
+import { useSelector, useDispatch } from "react-redux";
+import { signOutUserStart } from "./../../redux/User/user.actions";
 import "./styles.scss";
 import { Link } from "react-router-dom";
 import Logo from "./../../assets/logo.png";
@@ -13,8 +13,8 @@ const Header = (props) => {
   const dispatch = useDispatch();
   const { currentUser } = useSelector(mapState);
   const signOut = () => {
-    dispatch(signOutUserStart())
-  }
+    dispatch(signOutUserStart());
+  };
   return (
     <header className="header">
       <div className="wrap">
@@ -23,6 +23,16 @@ const Header = (props) => {
             <img src={Logo} alt="logo" />
           </Link>
         </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+          </ul>
+        </nav>
         <div className="callToActions">
           {currentUser && (
             <ul>
@@ -30,10 +40,7 @@ const Header = (props) => {
                 <Link to="/dashboard">My Account</Link>
               </li>
               <li>
-                <span
-                  style={{ cursor: "pointer" }}
-                  onClick={()=>signOut()}
-                >
+                <span style={{ cursor: "pointer" }} onClick={() => signOut()}>
                   LOGOUT
                 </span>
               </li>
@@ -61,5 +68,4 @@ Header.defaultProps = {
   currentUser: null,
 };
 
-
-export default Header
+export default Header;
--- src/components/ProductResults/Product/index.js
+++ src/components/ProductResults/Product/index.js
@@ -1,36 +1,37 @@
-import React from 'react'
-import './../styles.scss'
+import React from "react";
+import "./../styles.scss";
+import Button from "./../../Forms/Button";
 
-const Product = ({          productThumbnail,
-    productName,
-    productPrice,}) =>{
-        if (
-            !productThumbnail ||
-            !productName ||
-            typeof productPrice === "undefined"
-          )
-            return null;
-    return(
-        <div className="product">
-            <div className='thumb'>
-                <img src = {productThumbnail} alt = {productName} />
+const Product = ({ productThumbnail, productName, productPrice }) => {
+  if (!productThumbnail || !productName || typeof productPrice === "undefined")
+    return null;
+
+  const configAddToCartBtn = {
+    type: "button",
+  };
+  return (
+    <div className="product">
+      <div className="thumb">
+        <img src={productThumbnail} alt={productName} />
+      </div>
+
+      <div className="details">
+        <ul>
+          <li>
+            <span className="name"> {productName} </span>
+          </li>
+          <li>
+            <span className="price"> €{productPrice} </span>
+          </li>
+          <li>
+            <div className="addToCart">
+              <Button {...configAddToCartBtn}>Add to card </Button>
             </div>
-
-            <div className ='details'>
-                <ul>
-                    <li>
-                        <span className="name"> {productName} </span>
-                    </li>
-                    <li>
-                        <span className="price"> €{productPrice} </span>
-                    </li>
-                </ul>
-            </div>
-
-            </div>
-
-
-    )
-}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Product;
