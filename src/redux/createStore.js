69ec0048f5610fb60c64cae18ecab07072fb94d7
hamza bellamine <hamza.bellamine@gmail.com>
2021-02-04 14:17:23 +0100

    Redux persistor configuration- cart inchanged for user

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,17 +1,19 @@
-import {createStore,applyMiddleware} from 'redux'
-import logger from 'redux-logger'
-import createSagaMiddle from 'redux-saga'
-import rootReducer from './rootReducer';
-import rootSaga from './rootSaga'
-import thunk from 'redux-thunk' 
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import createSagaMiddle from "redux-saga";
+import rootReducer from "./rootReducer";
+import rootSaga from "./rootSaga";
+import thunk from "redux-thunk";
+import { persistStore } from "redux-persist";
 
-const sagaMiddleware = createSagaMiddle()
+const sagaMiddleware = createSagaMiddle();
 
-export const middlewares = [thunk,sagaMiddleware,logger]; // logger shows the events that happens in the redux store on the console
+export const middlewares = [thunk, sagaMiddleware, logger]; // logger shows the events that happens in the redux store on the console
 
-export const store = createStore(rootReducer,applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(rootSaga)
+sagaMiddleware.run(rootSaga);
 
-export default store;
- 
+export const persistor = persistStore(store);
+
+export default { store, persistor };
